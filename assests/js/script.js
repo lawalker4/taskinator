e5a3040b9febccfac888af58d355dcef7907ade6
LA WALKER <lauraashleywalker4@gmail.com>
2022-03-19 15:53:42 -0400

    add js button

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,21 @@
-console.dir(window.document);
-
 window.document.querySelector("button");
 
-var btn = window.document.querySelector('button');
+var btn = window.document.querySelector("button");
 console.dir(btn);
 
+document.querySelector('.btn')
+
+document.querySelector("button").textContent;
+
+document.querySelector("#save-task");
+
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+  });
+
+  document.createElement("li");
+
+  var taskItemEl = document.createElement("li");
